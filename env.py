08259cddf55078a7325c9662f6b9ea64dfbc5727
Shashwath-kumar <shashwath457@gmail.com>
2022-10-22 14:57:44 -0400

    Added graph2 and global var

--- env.py
+++ env.py
@@ -1,20 +1,99 @@
 # environment for Circle of Life
 import random
+import global_variables as g_v
 
 class Node:
-    def __init__(self, pos, agent, prey, predator) -> None:
+    def __init__(self, pos, agent = False, prey = False, predator = False) -> None:
         self.pos=pos
-        self.agent=False
-        self.prey=False
-        self.predator=False
+        self.agent = agent
+        self.prey = prey
+        self.predator = predator
+        self.neighbors = set()
+
+    def degree(self):
+        return len(self.neighbors)
+
+class Graph2:
+    def init_nodes(self):
+        #create new nodes
+        for i in range(self.numNodes):
+            new_node = Node(i)
+            self.graph_nodes.append(new_node)
+    
+    def make_circle(self):
+        #adds prev and next neighbor to each node
+        for i,node in enumerate(self.graph_nodes):
+            next_node = (i+1)%g_v.Number_of_nodes
+            prev_node = (i-1)%g_v.Number_of_nodes
+            node.neighbors.add(self.graph_nodes[next_node])
+            node.neighbors.add(self.graph_nodes[prev_node])
+
+    def node_selection(self, node: Node):
+        x=node.pos
+        potential_nodes=[]
+        for i in range(x-5, x+6):
+            temp = i % g_v.Number_of_nodes
+            temp_node = self.graph_nodes[temp]
+
+            if temp not in [x-1, x, x+1] and temp_node.degree() < 3:
+                potential_nodes.append(temp_node)
+        
+        # return a random potential node
+        if potential_nodes != []:
+            return random.choice(potential_nodes)
+        else:
+            return None
+
+    def add_new_edges(self):
+        indexes = list(range(g_v.Number_of_nodes))
+        while(len(indexes) > 0):
+            random_index = random.choice(indexes)
+            indexes.remove(random_index)
+            curr_node = self.graph_nodes[random_index]
+
+            if curr_node.degree() > 2:
+                continue
+
+            choice_node = self.node_selection(curr_node)
+            curr_node.neighbors.add(choice_node)
+            if choice_node:
+                choice_node.neighbors.add(curr_node)
+
+        # remove neigbors valued None
+        for node in self.graph_nodes:
+            if None in node.neighbors:
+                node.neighbors.remove(None)
+
+
+    def __init__(self) -> None:
+        self.numNodes = g_v.Number_of_nodes
+        # list to store the node objects
+        self.graph_nodes = list()
+        self.init_nodes()
+        self.make_circle()
+        self.add_new_edges()   
 
 class Graph:
+
+    def init_nodes(self):
+        #create new nodes
+        for i in range(self.numNodes):
+            new_node = Node(i)
+            self.graph_nodes.append(new_node)
+
+        #make circle
+        for i,node in enumerate(self.graph_nodes):
+            next_node = (i+1)%g_v.Number_of_nodes
+            prev_node = (i-1)%g_v.Number_of_nodes
+            node.neighbors.add(next_node)
+            node.neighbors.add(prev_node)
+
     def __init__(self) -> None:
-        self.numNodes = 50
+        self.numNodes = g_v.Number_of_nodes
         # list to store the node objects
         self.graphnodes = [None]
         for i in range(1, self.numNodes+1):
-            self.graphnodes.append(Node(i , False, False, False))
+            self.graphnodes.append(Node(i))
         self.graph = dict(zip(self.graphnodes[1:], [None]*self.numNodes))
 
     def node_selection(self, node: Node):
@@ -22,9 +101,9 @@ class Graph:
         potential_nodes=[]
         for i in range(x-5, x+6):
             if i < 1:
-                temp=50+i
-            elif i > 50:
-                temp=i-50
+                temp=g_v.Number_of_nodes+i
+            elif i > g_v.Number_of_nodes:
+                temp=i-g_v.Number_of_nodes
             else:
                 temp=i
             if temp!=x:
@@ -32,7 +111,7 @@ class Graph:
         # return a random potential node
         return random.choice(potential_nodes)
 
-    def edge_gen(self):
+    def edge_gen(self) -> dict():
         # generate a circular undirected graph
         self.graph[self.graphnodes[1]] = [self.graphnodes[self.numNodes], self.graphnodes[2]]
         for i in range(2, self.numNodes):
@@ -51,10 +130,13 @@ class Graph:
         return self.graph
 
 def main():
-    g=Graph()
-    graph=g.edge_gen()
-    for i, j in graph.items():
-        print(i.pos, [x.pos for x in j])
+    g = Graph2()
+    # graph = g.edge_gen()
+    # for i, j in graph.items():
+    #     print(i.pos, [x.pos for x in j])
+
+    for node in g.graph_nodes:
+        print(f"{node.pos}: ({[x.pos for x in node.neighbors]})")
 
 if __name__ == "__main__":
     main()
